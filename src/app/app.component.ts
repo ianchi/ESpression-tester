ffbf761f8688394c2909c51f877f4b8ebf2a75ec
Adrian Panella <ianchi74@outlook.com>
2018-01-27 11:59:26 -0600

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import {
+  es5ParserFactory, es5EvalFactory,
+  jsepParserFactory,
+  jsonPathEvalFactory, jsonPathParserFactory,
+  es5PathParserFactory
+} from 'espression';
+import { JsonPath } from 'espression/dist/src/eval/jsonPath';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,85 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  expression = 'a + b * c';
+  ast: any;
+  parser;
+  eval;
+  value: any;
+  context: any = {a: 1, b: 2, c: 3};
+  contextStr = '{"a": 1, "b": 2, "c": 3}';
+  builtins = true;
+  preset = 'ES5';
+
+  constructor() {
+    this.changePreset();
+    this.updateContext(null);
+  }
+
+  changePreset() {
+    switch (this.preset) {
+      case 'ES5':
+        this.parser = es5ParserFactory();
+        this.eval = es5EvalFactory();
+        break;
+      case 'jsep':
+        this.parser = jsepParserFactory();
+        this.eval = es5EvalFactory();
+        break;
+      case 'jsonPath':
+        this.parser = jsonPathParserFactory();
+        this.eval = jsonPathEvalFactory();
+        break;
+      case 'ES5+jP':
+        this.parser = es5PathParserFactory();
+        this.eval = jsonPathEvalFactory();
+        break;
+    }
+    this.updateExpression(null);
+  }
+  updateExpression(event?) {
+    try {
+      this.ast = this.parser.parse(this.expression);
+      this.value = this.eval.eval(this.ast, this.context);
+    } catch (e) {
+      this.ast = e.message;
+      this.value = undefined;
+    }
+
+    if (typeof this.value === 'undefined') { this.value = 'undefined'; }
+  }
+
+  updateContext(event?) {
+    try {
+      this.context = JSON.parse(this.contextStr);
+      if (typeof this.context !== 'object') { this.context = {}; }
+
+    } catch (e) {
+      this.context = {};
+    }
+    if (this.builtins) {
+      this.context = {
+        ... this.context, ...{
+          Object: Object,
+          Array: Array,
+          Math: Math,
+          Date: Date,
+          Number: Number,
+          String: String,
+          RegExp: RegExp,
+          Boolean: Boolean,
+
+          Infinity: Infinity,
+          undefined: undefined,
+          NaN: NaN,
+
+          isFinite: isFinite,
+          isNaN: isNaN,
+          parseFloat: parseFloat,
+          parseInt: parseInt
+        }
+      };
+    }
+    this.updateExpression(null);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 import { AppComponent } from './app.component';
 
+import {
+  MatFormFieldModule, MatInputModule, MatButtonModule, MatCardModule, MatExpansionModule,
+  MatCheckboxModule, MatRadioModule, MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatRadioModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+
+}
