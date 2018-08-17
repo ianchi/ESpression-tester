da7b000fc0c805a05580ca6eefe9ae8bb56ae87e
Adrian Panella <ianchi74@outlook.com>
2018-08-16 22:20:44 -0500

    fix: update to new espression api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,20 @@
 import { Component } from '@angular/core';
-import {
-  es5ParserFactory, es5EvalFactory,
-  jsepParserFactory,
-  jsonPathEvalFactory, jsonPathParserFactory,
-  es5PathParserFactory
-} from 'espression';
-import { JsonPath } from 'espression/dist/src/eval/jsonPath';
+import { ES5Parser, ES5StaticEval, BasicParser, Parser, StaticEval, INode } from 'espression';
+import { JsonPathParser, JsonPathStaticEval, ES5PathParser } from 'espression-jsonpath';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'app';
   expression = 'a + b * c';
-  ast: any;
-  parser;
-  eval;
+  ast: INode;
+  parser: Parser;
+  eval: StaticEval;
   value: any;
-  context: any = {a: 1, b: 2, c: 3};
+  context: any = { a: 1, b: 2, c: 3 };
   contextStr = '{"a": 1, "b": 2, "c": 3}';
   builtins = true;
   preset = 'ES5';
@@ -32,47 +27,51 @@ export class AppComponent {
   changePreset() {
     switch (this.preset) {
       case 'ES5':
-        this.parser = es5ParserFactory();
-        this.eval = es5EvalFactory();
+        this.parser = new ES5Parser();
+        this.eval = new ES5StaticEval();
         break;
       case 'jsep':
-        this.parser = jsepParserFactory();
-        this.eval = es5EvalFactory();
+        this.parser = new BasicParser();
+        this.eval = new ES5StaticEval();
         break;
       case 'jsonPath':
-        this.parser = jsonPathParserFactory();
-        this.eval = jsonPathEvalFactory();
+        this.parser = new JsonPathParser();
+        this.eval = new JsonPathStaticEval();
         break;
       case 'ES5+jP':
-        this.parser = es5PathParserFactory();
-        this.eval = jsonPathEvalFactory();
+        this.parser = new ES5PathParser();
+        this.eval = new JsonPathStaticEval();
         break;
     }
     this.updateExpression(null);
   }
-  updateExpression(event?) {
+  updateExpression(_event?) {
     try {
       this.ast = this.parser.parse(this.expression);
-      this.value = this.eval.eval(this.ast, this.context);
+      this.value = this.eval.evaluate(this.ast, this.context);
     } catch (e) {
       this.ast = e.message;
       this.value = undefined;
     }
 
-    if (typeof this.value === 'undefined') { this.value = 'undefined'; }
+    if (typeof this.value === 'undefined') {
+      this.value = 'undefined';
+    }
   }
 
-  updateContext(event?) {
+  updateContext(_event?) {
     try {
       this.context = JSON.parse(this.contextStr);
-      if (typeof this.context !== 'object') { this.context = {}; }
-
+      if (typeof this.context !== 'object') {
+        this.context = {};
+      }
     } catch (e) {
       this.context = {};
     }
     if (this.builtins) {
       this.context = {
-        ... this.context, ...{
+        ...this.context,
+        ...{
           Object: Object,
           Array: Array,
           Math: Math,
@@ -89,8 +88,8 @@ export class AppComponent {
           isFinite: isFinite,
           isNaN: isNaN,
           parseFloat: parseFloat,
-          parseInt: parseInt
-        }
+          parseInt: parseInt,
+        },
       };
     }
     this.updateExpression(null);
